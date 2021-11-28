f6b527735e06b80f7b0a4a1720d7a2d81bcf79f0
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-11-28 11:08:59 +0600

    Implements Socket in both side of client and server site

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const http = require('http')
 const expressServer = http.createServer(app);
 
+// Socket Configure
+const {Server} = require('socket.io');
+ const io = new Server(expressServer);   //made socket server with respect to expressServer
+
+
 app.get('/', function(req, res){
     res.sendFile(__dirname+"/index.html")
 })

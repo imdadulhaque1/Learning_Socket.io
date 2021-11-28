ad80ba006bb48fdc878d5f17fcf9c1e2626eafab
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-11-28 12:27:33 +0600

    Data Through from server

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const app = express();
 const http = require('http');
 const expressServer = http.createServer(app);
 
-// Socket Configure
+//--->Socket Configure
 const {Server} = require('socket.io');
 const io = new Server(expressServer);   //made socket server with respect to expressServer
 
-// User Connection and Disconnected Status
+//--->User Connection and Disconnected Status
 io.on('connection', function(socket){
     console.log("New User Connected!");
 
-    socket.on('disconnect', function(){
-        console.log("User disconnected!")
-    })
+    //--->User Disconnection Status
+    // socket.on('disconnect', function(){
+    //     console.log("User disconnected!")
+    // })
+
+    // Data Send Server to Client (Data through ----> )
+    setTimeout(function(){
+        socket.send("Data sending flow status from server to client(Server --> Client)!")
+    }, 10000)
+
 })
 
 app.get('/', function(req, res){
